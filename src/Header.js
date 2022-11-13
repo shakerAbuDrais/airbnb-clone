d69ef826ba5902cb7e75bb618844ca38e2edee16
shakerAbuDrais <thecoffeiner@gmail.com>
2022-11-14 00:06:16 +0300

    Create Home Page

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <div className="header">
+      <img className="header__icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Airbnb_Logo_B%C3%A9lo.svg/1200px-Airbnb_Logo_B%C3%A9lo.svg.png" alt="" width="100px" />
+    </div>
+  );
+}
+
+export default Header;
